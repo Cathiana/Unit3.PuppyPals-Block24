0d396cb5035bda1587d0cb4d568408fd5b437f67
Cathiana <cathianao@gmail.com>
2024-02-16 11:12:40 -0500

    issued changes in code - set puppies isnt complying

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,48 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { puppyList } from './data.js'
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
+  const [puppies, setPuppies] = useState(puppyList);
+   //create piece of state but I'll call it puppies
+  //set Puppies -setter function
+  //inset in the useState (initial value: array of data)
+  //equal to the puppy list in data.js - actual value to assign
+  const [featPupId, setFeatPupId] = useState(null);
+  //null is helpful with conditional rendering 
+  //Array.find = Line 15 
+  const featuredPup = puppies.find((puppy) => puppy.id === featPupId)
+
+  //what should we do in handleclick? 
+  //components reload and show additoinal information
+  //change the featured puppy id
+
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <div className="App">
+        <p>Featured puppy: {featPupId}</p>
+      
+       {featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+    
 
+
+      {
+        puppies.map((puppy) => {
+        return <p onClick={() => {
+      setFeatPupId(puppy.id)
+     }} key={puppy.id}>{puppy.name}</p>;
+   })
+} 
+      </div>
+  );
+}
 export default App
